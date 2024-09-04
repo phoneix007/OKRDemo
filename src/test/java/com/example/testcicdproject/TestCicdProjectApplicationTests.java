537bda63da289eb8334b7bd4ff408d31f27a4cf7
SharmaAjay <ajaysharma520420@gmail.com>
2024-09-05 01:37:16 +0530

    Initial Commit

---
+++ src/test/java/com/example/testcicdproject/TestCicdProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testcicdproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCicdProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
